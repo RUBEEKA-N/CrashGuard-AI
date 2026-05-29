5ce30e907a2da29483a31746a7ca61eca029210e
Rubeeka <rubeerubeeka@gmail.com>
2026-05-29 20:13:02 +0530

    Added website

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function detectAccident(){
+
+document.getElementById("alertBox").innerHTML =
+
+"🚨 Accident Detected! <br><br> 📍 Location Shared <br><br> 🏥 Nearby Hospital Alert Sent <br><br> 🚑 Ambulance On The Way";
+
+}
