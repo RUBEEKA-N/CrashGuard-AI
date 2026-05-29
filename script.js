08f8a19a80ad5a765c494d26dd7a470230cba863
Rubeeka <rubeerubeeka@gmail.com>
2026-05-29 20:44:55 +0530

    Updated hospital alert system

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-
 function detectAccident(){
 
 document.getElementById("alertBox").innerHTML =
 
-"🚨 Accident Detected! <br><br> 📍 Location Shared <br><br> 🏥 Nearby Hospital Alert Sent <br><br> 🚑 Ambulance On The Way";
+`
+<h2>🚨 Accident Detected</h2>
+
+<div class="hospital">
+🏥 Apollo Hospital - 2.1 km <br>
+🚑 Ambulance Dispatched
+</div>
+
+<div class="hospital">
+🏥 MIOT Hospital - 4.3 km <br>
+🟢 Trauma Bed Available
+</div>
+
+<div class="hospital">
+🏥 Global Hospital - 5.0 km <br>
+📞 Emergency Team Alert Sent
+</div>
+
+`;
 
 }
